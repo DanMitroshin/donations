60d8ffc9fa60f4044899478aa12cc9ca509c7440
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2020-10-18 15:51:59 +0300

    Updates

---
+++ precache-manifest.c66822c6d0e780e550cee30249d304d6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726c0dfbf007841274a7269fbe3fcd8b",
+    "url": "/donations/index.html"
+  },
+  {
+    "revision": "817277f8f36675d9f0c9",
+    "url": "/donations/static/css/main.8646b007.chunk.css"
+  },
+  {
+    "revision": "3cc5df19b68ff6936374",
+    "url": "/donations/static/js/2.51d2b15f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/donations/static/js/2.51d2b15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817277f8f36675d9f0c9",
+    "url": "/donations/static/js/main.38f34ace.chunk.js"
+  },
+  {
+    "revision": "f14b27c5aa9a23131fd0",
+    "url": "/donations/static/js/runtime-main.9fe3a625.js"
+  },
+  {
+    "revision": "59d60af3c22a11a2bf842cd9490afb2b",
+    "url": "/donations/static/media/Circe-Bold.59d60af3.ttf"
+  },
+  {
+    "revision": "6c59e123427d03197dbcddd8e5506deb",
+    "url": "/donations/static/media/Circe-Bold.6c59e123.eot"
+  },
+  {
+    "revision": "977dee27389906fcdb91575955cf543c",
+    "url": "/donations/static/media/Circe-Bold.977dee27.woff"
+  },
+  {
+    "revision": "8736804787b67fa125b215f86d3b66bd",
+    "url": "/donations/static/media/Circe-ExtraBold.87368047.woff"
+  },
+  {
+    "revision": "8d9ba51f3f1ecf7c58580ccc56adc490",
+    "url": "/donations/static/media/Circe-ExtraBold.8d9ba51f.ttf"
+  },
+  {
+    "revision": "b325a34030a457c284f193c8c64bb1ef",
+    "url": "/donations/static/media/Circe-ExtraBold.b325a340.eot"
+  },
+  {
+    "revision": "171fe7df080d14fe10ea9a6f33bf92b1",
+    "url": "/donations/static/media/Circe-ExtraLight.171fe7df.woff"
+  },
+  {
+    "revision": "2ff6670b8281283051726cf25cb9b78b",
+    "url": "/donations/static/media/Circe-ExtraLight.2ff6670b.eot"
+  },
+  {
+    "revision": "7fc3ecc069378d95e98f69482de84ae2",
+    "url": "/donations/static/media/Circe-ExtraLight.7fc3ecc0.ttf"
+  },
+  {
+    "revision": "35b2348d354d083173d9b7dac3fd6523",
+    "url": "/donations/static/media/Circe-Light.35b2348d.ttf"
+  },
+  {
+    "revision": "85c00974099ece870bd68c33da68d411",
+    "url": "/donations/static/media/Circe-Light.85c00974.woff"
+  },
+  {
+    "revision": "de6af660eac77fdd3d5bcbbb67a115be",
+    "url": "/donations/static/media/Circe-Light.de6af660.eot"
+  },
+  {
+    "revision": "7786e397b9971d35f3893a1dec2b1b3f",
+    "url": "/donations/static/media/Circe-Regular.7786e397.eot"
+  },
+  {
+    "revision": "c7d081a3dfb9356de83ac79a28ec13d4",
+    "url": "/donations/static/media/Circe-Regular.c7d081a3.ttf"
+  },
+  {
+    "revision": "e8c2bdb5866b55bb52b64b8ea8b4a38d",
+    "url": "/donations/static/media/Circe-Regular.e8c2bdb5.woff"
+  },
+  {
+    "revision": "c8b6116e2255952ba80556d8c3ae0596",
+    "url": "/donations/static/media/Circe-Thin.c8b6116e.ttf"
+  },
+  {
+    "revision": "ce6ea34285d46fc250e50bd72fdb4255",
+    "url": "/donations/static/media/Circe-Thin.ce6ea342.woff"
+  },
+  {
+    "revision": "d9851189d1838977014feb34fb4eac8a",
+    "url": "/donations/static/media/Circe-Thin.d9851189.eot"
+  }
+]);
